6e691f0f70e4da97febe61d6e3faaf4526298390
Seok-Woo Han <h9379203@gmail.com>
2019-06-25 15:18:51 +0000

    base

---
+++ src/MainClass.java
@@ -0,0 +1,14 @@
+package src;
+
+public class MainClass
+{
+	public String name;
+	
+	public void MainClass(){
+		System.out.println("Constructor");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+};
